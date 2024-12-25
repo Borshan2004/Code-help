7c1869422b4f8c2fb5065f230e3d745fef4dbcbf
Borshan2004 <borshanpurification42@gmail.com>
2024-12-25 19:36:49 +0600

    Add files via upload

---
+++ Variables and DataTypes in JS || JavaScript Series 2024/java.js
@@ -0,0 +1,32 @@
+var age = 25;
+
+if (true) {
+    console.log(age);
+}
+
+
+
+{
+    let a = 200
+    console.log(a)
+}
+
+
+
+const c = 28;
+console.log(c)
+c = 20
+
+let marks;
+console.log(marks)
+
+
+let value1 = null;
+console.log(value1)
+
+let value2 = 12345678901213141516171819202131415161718191910262525242578237286487684732684763487643876587643287637843;
+console.log(value2)
+
+
+marks = false;
+console.log(typeof(marks))
