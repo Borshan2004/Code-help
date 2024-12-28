30ce5abcdfe660988c442bcbc05c96516f5e8952
Borshan2004 <borshanpurification42@gmail.com>
2024-12-28 21:47:27 +0600

    Add files via upload

---
+++ Operators & Conditionals in JavaScript || JavaScript Series 2024/java.js
@@ -0,0 +1,109 @@
+let a = 6
+let b = 5
+// console.log(a+b);
+// console.log(b-a);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(b%a);
+// console.log(a**b);
+
+// a = a + 10
+// console.log(a)
+
+// a+= 10
+// console.log(a)
+
+b+=10
+console.log(b)
+
+
+// c = a-2
+// console.log(c)
+
+// a-=2
+// console.log(a)
+
+
+// a*=6
+// console.log(a)
+
+
+
+
+// console.log(50 > 5);
+// console.log(50<5);
+// console.log(50 >= 5);
+// console.log(50 <= 5);
+// console.log(5 == 5);
+// console.log("5" == 5);
+// console.log("5" === 5);
+// console.log(5 === 5);
+
+
+
+
+
+
+
+
+
+
+// let age =102;
+// let atatus1 = (age>18) ?  " I can vote" :"I can not vote "; 
+// console.log(atatus1);
+
+
+// let ans = (true&&true&&true)
+// console.log(ans);
+
+// let ans1 = (true&&false&&true)
+// console.log(ans1);
+
+// let ans2 = (true||false||true)
+// console.log(ans2);
+
+// let ans3 = (false||false||false)
+// console.log(ans3);
+
+// let ans4 = !(true)
+// console.log(ans4);
+
+
+// let ans5 = !(false)
+// console.log(ans5);
+
+
+
+// console.log(false||"borshsan")
+// console.log(2 & 5);
+// console.log(2 | 5);
+// console.log(~0);
+// console.log(2^2);
+// console.log(2^3);
+// console.log(10>>1);
+// console.log(10<<1);
+
+
+
+
+borshan = 178
+
+// if (borshan>18)
+// {
+//     console.log("hello please give your vote")
+
+// }
+// else
+// {
+//     console.log("please dont vote")
+// }
+
+
+ let bp = 2;
+switch(bp)
+{
+    case 1: console.log("a");
+    case 2: console.log("b");
+    break
+    case 3: console.log("c"); 
+}
