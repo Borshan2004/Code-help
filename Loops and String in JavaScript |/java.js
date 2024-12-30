9563a48746734c52ceb49463f9c0976f166e4b26
Borshan2004 <borshanpurification42@gmail.com>
2024-12-30 20:42:26 +0600

    Add files via upload

---
+++ Loops and String in JavaScript |/java.js
@@ -0,0 +1,119 @@
+// for(let i=1;i<=60;i=i+1)
+// {
+//     console.log("borhsan");
+// }
+
+
+
+// for(let i=1;i<=5;i=i+1)
+// {
+//     console.log(i);
+// }
+
+
+
+
+
+// for(let i=5;i>0;i=i-1)
+// {
+//     console.log(i);
+// }
+
+
+
+
+
+// for(let i=1;i<=6;i++){
+//     if(i==4){
+//         break;
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+
+
+// let i =1;
+// while(i<=5){
+//     console.log("borshan");
+//     i=i+1;
+// }
+
+
+// let i =1;
+// while(i<=6){
+//     console.log(i);
+//     i=i+1;
+// }
+
+
+// let i=5;
+// while(i>0){
+//     console.log(i);
+//     i=i-1
+// }
+
+
+// let i=1;
+// while(i<5){
+//     console.log("hello");
+//     if(i==3)
+//         continue;
+//     else
+//        console.log("borshan");
+//     i=i+1
+// }
+
+// let i = 1;
+// while(i<5){
+//     console.log("inside the input");
+//     if(i==3){
+//         i=i+1;
+//         continue;
+//     }
+//     else
+//        console.log(i);
+//        i=i+1
+// }
+
+
+
+// let i=1;
+// do{
+//     console.log("borshan");
+//     i=i+1;
+// }while(i<=10);
+
+
+ 
+
+// let na = "borno"
+
+// console.log(na);
+
+
+
+// let n =`hello
+// how are you
+// what you want to do it 
+// do you know how to work for it`
+
+// console.log(n);
+
+
+
+// let op1 = "Borshan ";
+// let op2 ="Purificat";
+// // let ans = op1   +   op2
+// let answer = `${op1}${op2}`
+// console.log(answer);
+
+// console.log(op2.length)
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+
+
+let str ="borshan"
+console.log(str.substring(2,5))
