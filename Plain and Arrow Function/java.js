e7bc6ac93e19c18d0475f172d29291506bbcaeab
Borshan2004 <borshanpurification42@gmail.com>
2024-12-31 18:59:09 +0600

    Add files via upload

---
+++ Plain and Arrow Function/java.js
@@ -0,0 +1,54 @@
+// function myName()
+// {
+//     console.log("My name is borshan")
+// }
+
+
+// myName();
+
+
+
+// function countingName(){
+//     for(let i=1;i<=100;i=i+1){
+//         console.log(i);
+//     }
+// }
+
+// countingName();
+
+
+
+// function paranum(num){
+//        console.log("the priniting number",num)
+// }
+
+// paranum(4);
+
+
+
+// function getAverage(num1,num2){
+//     let average = (num1+num2)/2;
+//     console.log("Print the average of the number is :",average);
+// }
+
+// getAverage(10,8);
+
+
+
+// function getSum(a,b,c){
+//     let sum = (a+b+c);
+//     return sum;
+// }
+
+// let ans = getSum(3,3,3);
+// console.log("print the number",ans)
+
+
+
+function muntification(x,y){
+    let ansm =x*y;
+    return ansm;
+}
+
+let ans2 = muntification(5,3);
+console.log(ans2);
