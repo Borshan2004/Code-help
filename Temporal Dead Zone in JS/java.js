15ea9a20bab7f9b0b8eb7bd3c8f31049f6f399d2
Borshan2004 <borshanpurification42@gmail.com>
2025-01-06 13:35:35 +0600

    Add files via upload

---
+++ Temporal Dead Zone in JS/java.js
@@ -0,0 +1,25 @@
+// var age = 20;
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true){
+//     console.log(age);
+// }
+
+// for(var i= 0 ; i<2 ; i=i+1) {
+//    console.log(age);
+// }
+
+
+function sayHello(){
+    var fullname = "borshan";
+    console.log("hello my name is boss");
+    console.log(fullname);
+}
+
+
+
+sayHello();
