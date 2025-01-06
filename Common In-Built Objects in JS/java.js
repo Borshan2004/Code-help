0a900c037579907b54d43fefcecbe8b6174e63fa
Borshan2004 <borshanpurification42@gmail.com>
2025-01-06 23:15:52 +0600

    Add files via upload

---
+++ Common In-Built Objects in JS/java.js
@@ -0,0 +1,16 @@
+console.log(Math.PI);
+console.log(Math.max(200,30,2561,5615,1500,2356));
+console.log(Math.min(200,30,2561,5615,1500,2356));
+console.log(Math.abs(-250));
+console.log(Math.round(1.9));
+
+
+let time = new Date();
+console.log(time);
+
+
+let newDate = new Date(1998,5,20,7)
+console.log(newDate.getFullYear());
+
+
+
