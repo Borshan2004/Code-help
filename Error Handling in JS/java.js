25a7bd6871798eed26f9cd5b7bc1f0e0faa5abcc
Borshan2004 <borshanpurification42@gmail.com>
2025-01-07 18:01:08 +0600

    Add files via upload

---
+++ Error Handling in JS/java.js
@@ -0,0 +1,33 @@
+// console.log(x)
+
+// try{
+//     console.log("hello i am borshan ");
+//     console.log("To day I am Learning how to handle the error of the program");
+//     console.log(x);
+//     console.log("To day I am Learning how to handle the error of the program");
+
+// }
+
+// catch(err){
+//     console.log("Sorry there is a tecnical problem so that the website is not working",err);
+     
+// }
+// finally{
+//     console.log("There is many problem in the program but me finally always print");
+// }
+
+
+// try{  
+//     console.log(x);
+
+// }
+
+// catch(e){
+//     throw new Error("this error is came from borshan");
+
+// }
+
+let errorCode = 100;
+if(errorCode == 100) {
+    throw new Error("this is from big ball");
+}
